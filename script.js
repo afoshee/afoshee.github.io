e1222163c33b48e17b030e0a4271a708fa22465a
Anna Foshee <anna.fosh@gmail.com>
2020-11-17 19:45:49 -0500

    add buttons with show/hideJS function

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+
+function dropDown() {
+    var x = document.getElementById("higherEd");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function dropDown2() {
+    var x = document.getElementById("culture");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function dropDown3() {
+    var x = document.getElementById("nonProfit");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
 function validateForm() {
     var x = document.forms["contactform"] ["fullname"].value;
     if (x == "") {
@@ -6,4 +34,3 @@ function validateForm() {
     }
     document.write("Your inquiry has been submitted. Thank you.");
 }
-
